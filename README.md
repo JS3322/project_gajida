6aa57f695c7f539e1d870e130662f74f8c43da58
js <bada@ganada.kr>
2021-10-02 14:06:18 +0900

    20211002 init project gajida

---
+++ README.md
@@ -0,0 +1,29 @@
+```
+PROJECT GAJIDA
+```
+---
+### Package Architecture
+####calculator
+1. | test
+2. | test
+
+---
+### Log
+20211001 init project gajida
+
+## Stay in touch
+
+- Author - [JS KIM](https://cleancode.kr)
+
+## Support
+
+[read more here](https://cleancode.kr).
+
+## License
+
+Nest is [MIT licensed](LICENSE).
+
+---
+*name : JS  
+*page : cleancode.kr    
+*email : bada@ganada.kr
---
+++ main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def search():
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.get("https://google.com")
+
+    search_bar = browser.find_element_by_class_name("gLFyf")
+
+    search_bar.send_keys("hello!")
+    search_bar.send_keys(Keys.ENTER)
+
+    search_results = browser.find_elements_by_class_name("g")
+
+    return print(search_results)
+
+
+search()
