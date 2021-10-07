bed382bd5c339b9efcc307fd13dfe4e837c604b7
js <bada@ganada.kr>
2021-10-07 22:48:22 +0900

    20211007 add .gitignore

--- acquiring/main.py
+++ acquiring/main.py
@@ -2,19 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
+KEYWORD = "buy domain"
 
-def search():
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    browser.get("https://google.com")
+browser = webdriver.Chrome(ChromeDriverManager().install())
 
-    search_bar = browser.find_element_by_class_name("gLFyf")
+browser.get("https://google.com")
 
-    search_bar.send_keys("hello!")
-    search_bar.send_keys(Keys.ENTER)
+search_bar = browser.find_element_by_class_name("gLFyf")
 
-    search_results = browser.find_elements_by_class_name("g")
+search_bar.send_keys(KEYWORD)
+search_bar.send_keys(Keys.ENTER)
 
-    return print(search_results)
+search_results = browser.find_elements_by_class_name("g")
+
+for index, search_result in enumerate(search_results):
+    class_name = search_result.get_attribute("class")
+    if "kno-kp mnr-c g-blk" not in class_name:
+        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
 
 
-search()
+browser.quit()
