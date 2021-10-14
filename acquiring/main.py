f75314b69ba048ebcb60d7922ebd932135ffd232
js <bada@ganada.kr>
2021-10-14 12:20:04 +0900

    20211014 fix main.py

--- acquiring/main.py
+++ acquiring/main.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome import options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,9 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 KEYWORD = "buy domain"
-
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -20,6 +16,20 @@ search_bar = browser.find_element_by_class_name("gLFyf")
 search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
+
 shitty_element = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "Wt5Tfe")))
+    EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
+)
+
 print(shitty_element)
+
+
+""" search_results = browser.find_elements_by_class_name("g")
+
+for index, search_result in enumerate(search_results):
+    class_name = search_result.get_attribute("class")
+    if "kno-kp mnr-c g-blk" not in class_name:
+        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+ """
+
+# browser.quit()
